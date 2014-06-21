1a1d1e0e050f219691f565b71f653308291a21e5
professir <buckcyoung@gmail.com>
2014-06-21 16:19:17 -0400

    f

--- src/scripts/Localize.py
+++ src/scripts/Localize.py
@@ -4,9 +4,10 @@ import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import *
 from cs1567p2.msg import *
 
-RED   = [[148, 126, 229]]
-GREEN = [[212, 241, 219]]
-color_mask_list = [[148, 126, 229], [212, 241, 219]]
+RED   = [148, 126, 229]
+GREEN = [212, 241, 219]
+BLUE  = [225, 174, 140]
+color_mask_list = [GREEN, RED, BLUE]
 threshold = 50
 locpub = None
 kinect1pub = None
