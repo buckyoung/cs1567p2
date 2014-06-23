26bed9b2effbfa083ea125a8adf7fde2f268a19b
Buck Young <buckcyoung@gmail.com>
2014-06-22 20:15:01 -0400

    this sucks

--- src/scripts/Localize.py
+++ src/scripts/Localize.py
@@ -3,6 +3,7 @@ import rospy
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import *
 from cs1567p2.msg import *
+from collections import deque
 
 #TAPE   = [240, 243, 244]
 #RED    = [148, 126, 229] # Not bad
@@ -10,33 +11,69 @@ from cs1567p2.msg import *
 #BLUE   = [224, 174, 139] # Not bad -- lost on k2 sometimes
 #ORANGE = [156, 201, 243] 
 #BROWN  = [142, 179, 211] # Not terrible -- steals some red
-#BLACK  = [112, 99, 97]
+BLACK  = [0,0,0]
 #YELLOW = [165, 251, 253] # Not bad -- NEON ORANGE POST IT!, also yellow and some orange
 #WHITE  = [255, 255, 255] #Really good, except computer tops
 #Green and Orange dont play nice
 #Red and Brown dont play nice
-LIZLTRED  = [89,85,225]
+RED  = [89,85,225]
 #LIZDKRED  = [56,56,166] #*having problems
-LIZGREEN  = [145,195,97] #*having problems ?? IS THIS ORANGE?
-LIZLTPURP = [210,160,198]
+#LIZGREEN  = [145,195,97] #*having problems ?? IS THIS ORANGE?
+LTPURP = [210,160,198]
 #LIZDKPURP = [122,83,105] #terrible - catches a lot of carpet
-LIZBLUE   = [227,186,88] #note: also picks up TARGBLUE
+BLUE   = [227,186,88] #note: also picks up TARGBLUE
 #TARGETS:
 #TARGGREEN  = [95,135,102]
 #TARGYELLOW = [120, 243, 250]
 #TARGORANGE = [99,161,235]
 #TARGBLUE = [222,201,79]
-TARGPURPLE = [173,108,137]
+DRKPURP = [173,108,137]
 #TARGBROWN = [104,145,198]
 #TARGTAN   = [155,214,243]
-color_mask_list = [LIZLTRED, LIZLTPURP, LIZBLUE, TARGPURPLE, LIZGREEN]
-threshold = 30
+color_mask_list = [BLUE, RED, DRKPURP, LTPURP]
+threshold = 35
 locpub = None
 kinect1pub = None
 kinect2pub = None
 kinect3pub = None
-top_mask = Image()
-mid_mask = Image()
+_1_mask = Image()
+_2_mask = Image()
+_3_mask = Image()
+blob_queue_2 = deque() 
+
+class Point:
+    row = -1
+    col = -1
+class ColorPoint:
+    row = -1
+    col = -1
+    color = [-1, -1, -1]
+    def __init__(self, otherpoint=None):
+        if(otherpoint != None):
+            self.row = otherpoint.row
+            self.col = otherpoint.col
+            self.color = otherpoint.color
+class Blob:    
+    start = ColorPoint()
+    color = [-1, -1, -1]
+    min_row = -1
+    max_row = -1
+    min_col = -1
+    max_col = -1
+    num_px = 0
+    center = Point()
+    mask = Image()
+    def __init__(self, start_cp, mask):
+        self.start   = start_cp
+        self.color   = start_cp.color
+        self.min_row = start_cp.row
+        self.max_row = start_cp.row
+        self.min_col = start_cp.col
+        self.max_col = start_cp.col
+        self.num_px  = 0
+        self.center.row = start_cp.row
+        self.center.col = start_cp.col
+        self.mask = mask
 
 def print_color(message):
     global kinect3pub
@@ -72,16 +109,16 @@ def print_color(message):
 
 def _1_image_callback(message):
     global color_mask_list
-    global top_mask
+    global _1_mask
     global threshold
     global kinect1pub
     #make a new image if you want to view your mask
-    top_mask = Image()
-    top_mask.height = message.height
-    top_mask.width = message.width
-    top_mask.encoding = message.encoding
-    top_mask.is_bigendian = message.is_bigendian
-    top_mask.step = message.step
+    _1_mask = Image()
+    _1_mask.height = message.height
+    _1_mask.width = message.width
+    _1_mask.encoding = message.encoding
+    _1_mask.is_bigendian = message.is_bigendian
+    _1_mask.step = message.step
     if message.encoding == "bgr8": #this is image_color encoding
         byte_array = list(message.data) #convert unit8[] from string to chars
         mask_array = list(message.data)
@@ -103,22 +140,21 @@ def _1_image_callback(message):
                     mask_array[3*index+0] = chr(207) #
                     mask_array[3*index+1] = chr(207) #
                     mask_array[3*index+2] = chr(207) #
-    top_mask.data = "".join(mask_array) #make char[] back into uint8[] string
-    kinect1pub.publish(top_mask) #publish the mask for viewing
-    print "\a" #DEBUG (sound the alarm)
+    _1_mask.data = "".join(mask_array) #make char[] back into uint8[] string
+    kinect1pub.publish(_1_mask) #publish the mask for viewing
     print "Top, Pic 1 Published!"
         
 def _2_image_callback(message):
     global color_mask_list
-    global mid_mask
+    global _2_mask
     global threshold
     global kinect2pub
-    mid_mask = Image()
-    mid_mask.height = message.height
-    mid_mask.width = message.width
-    mid_mask.encoding = message.encoding
-    mid_mask.is_bigendian = message.is_bigendian
-    mid_mask.step = message.step
+    _2_mask = Image()
+    _2_mask.height = message.height
+    _2_mask.width = message.width
+    _2_mask.encoding = message.encoding
+    _2_mask.is_bigendian = message.is_bigendian
+    _2_mask.step = message.step
     if message.encoding == "bgr8":
         byte_array = list(message.data)
         mask_array = list(message.data)
@@ -136,22 +172,22 @@ def _2_image_callback(message):
                     mask_array[3*index+0] = chr(207) #
                     mask_array[3*index+1] = chr(207) #
                     mask_array[3*index+2] = chr(207) #
-    mid_mask.data = "".join(mask_array)
-    kinect2pub.publish(mid_mask)
-    print "\a" #DEBUG (sound the alarm)
+    _2_mask.data = "".join(mask_array)
+    kinect2pub.publish(_2_mask)
     print "Bottom, Pic 2 Published!"
+    localize(_2_mask)
 
 def _3_image_callback(message):
     global color_mask_list
-    global mid_mask
+    global _3_mask
     global threshold
     global kinect3pub
-    mid_mask = Image()
-    mid_mask.height = message.height
-    mid_mask.width = message.width
-    mid_mask.encoding = message.encoding
-    mid_mask.is_bigendian = message.is_bigendian
-    mid_mask.step = message.step
+    _3_mask = Image()
+    _3_mask.height = message.height
+    _3_mask.width = message.width
+    _3_mask.encoding = message.encoding
+    _3_mask.is_bigendian = message.is_bigendian
+    _3_mask.step = message.step
 
     if message.encoding == "bgr8":
         byte_array = list(message.data)
@@ -170,11 +206,100 @@ def _3_image_callback(message):
                     mask_array[3*index+0] = chr(207) #
                     mask_array[3*index+1] = chr(207) #
                     mask_array[3*index+2] = chr(207) #
-    mid_mask.data = "".join(mask_array)
-    kinect3pub.publish(mid_mask)
-    print "\a" #DEBUG (sound the alarm)
+    _3_mask.data = "".join(mask_array)
+    kinect3pub.publish(_3_mask)
     print "Middle, Pic 3 Published!"
+    localize(_3_mask)
 
+def localize(mask):
+    color_point = ColorPoint()
+    #robot_colors = [DRKPURP]
+    # iterate thru mask to find a certain color (Each robot will have its own color)
+    #for color in robot_colors:
+    color_point = find_color(mask, BLUE)
+    blob = Blob(color_point, mask)
+    print("starting fill blob")
+    blob = fill_blob(blob, blob.start)
+    print("finished fill blob, debug pub")
+    global kinect2pub
+    kinect2pub.publish(blob.mask)
+    if blob.num_px > 100:
+        #found our robot!
+        color_point = ColorPoint()
+    # fill color to make blob
+    # is blob big enough to be a square?
+    # yes, search around blob for the orientation color (same for each robot)
+    # is orientation blob big enough to be square?
+    # yes, we have a robot and an orientation
+def find_color(mask, color):
+    row = -1
+    col = -1
+    for index in xrange(mask.width*mask.height):
+        if (mask.data[3*index+0] == chr(color[0])\
+                and mask.data[3*index+1] == chr(color[1])\
+                and mask.data[3*index+2] == chr(color[2])):
+            # if the data pixel matches the color
+            row = int(index/mask.width)
+            col = index % mask.width
+            break
+    result = ColorPoint()
+    result.row = row
+    result.col = col
+    result.color = color
+    return result
+    
+#accept a blob and a point
+#recursively check neighbors to fill
+#return a blob
+def fill_blob(blob, cpoint):
+    mask_array = list(blob.mask.data)
+    #1) Check if target color
+    if not(ord(mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+0]) == blob.color[0]\
+            and ord(mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+1]) == blob.color[1]\
+            and ord(mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+2]) == blob.color[2]):
+        return blob
+    # our target color is the blob color
+    # set this to visited on our blobs mask
+    mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+0] = chr(0)
+    mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+1] = chr(0)
+    mask_array[3*(cpoint.row*blob.mask.width+cpoint.col)+2] = chr(0)
+    blob.mask.data = "".join(mask_array)
+    # init new points:
+    u = ColorPoint(cpoint)
+    d = ColorPoint(cpoint)
+    l = ColorPoint(cpoint)
+    r = ColorPoint(cpoint)
+    u.row = u.row - 1
+    d.row = d.row + 1
+    r.col = r.col + 1
+    l.col = l.col - 1
+    ur     = ColorPoint(u)
+    ur.col = ur.col + 1
+    ul     = ColorPoint(u)
+    ul.col = ul.col - 1
+    dr     = ColorPoint(d)
+    dr.col = dr.col + 1
+    dl     = ColorPoint(d)
+    dl.col = dl.col - 1
+    #ok, new points are set 
+    if u.row >= 0:
+        blob = fill_blob(blob, u)
+    if ur.row >= 0 and ur.col < blob.mask.width:
+        blob = fill_blob(blob, ur)
+    if r.col < blob.mask.width:
+        blob = fill_blob(blob, r)
+    if dr.row < blob.mask.height and dr.col < blob.mask.width:
+        blob = fill_blob(blob, dr)
+    if d.row < blob.mask.height:
+        blob = fill_blob(blob, d)
+    if dl.row < blob.mask.height and dl.col >= 0:
+        blob = fill_blob(blob, dl)
+    if l.col >= 0:
+        blob = fill_blob(blob, l)
+    if ul.row >= 0 and ul.col >= 0:
+        blob = fill_blob(blob, ul)
+    return blob
+    
 def _1_cloud_callback(message):
     try:
         #make a generator, skipping points that have no depth, on points in 
@@ -224,7 +349,7 @@ def initialize():
     #rospy.Subscriber("/kinect1/depth_registered/points", PointCloud2, _1_cloud_callback)
     rospy.Subscriber("/kinect2/rgb/image_color", Image, _2_image_callback)
     #rospy.Subscriber("/kinect2/depth_registered/points", PointCloud2, _2_cloud_callback)
-    rospy.Subscriber("/kinect3/rgb/image_color", Image, _3_image_callback)
+#    rospy.Subscriber("/kinect3/rgb/image_color", Image, _3_image_callback)
     #rospy.Subscriber("/kinect3/depth_registered/points", PointCloud2, _3_cloud_callback)
 #    rospy.Subscriber("/kinect3/rgb/image_color", Image, print_color)
     rospy.spin()
